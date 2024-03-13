56026eb727c38ce273b56025999082285c95ee48
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-03-13 20:44:58 +0700

    feat: add menu scene

--- scenes/Menus/Menu.gd
+++ scenes/Menus/Menu.gd
@@ -8,8 +8,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/Start.grab_focus()
-
+	$BlackNoir.play('default')
+	$WhiteNoir.play('default')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
